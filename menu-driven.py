e4a509129c00d7bca547559913215c2bfb57d49e
subekshya-s <subekshyasubedi26@gmail.com>
2026-03-12 02:53:40 +0000

    feat: Add the choice based for each task in the utility system

---
+++ menu-driven.py
@@ -0,0 +1,32 @@
+while True:
+    print("\n---------------MENU------------------")
+    print("1 : BMI Calculator")
+    print("2 : Taxation System")
+    print("3 : Currency Converter")
+    print("4 : Remittance")
+    print("5 : Lagani Kosh")
+    print("6 : Exit")
+
+    choice = input("Enter your choice (1-6): ")
+
+    if choice == "1":
+        print("https://github.com/bidhyabhattarai/BMI_calculator")
+
+    elif choice == "2":
+        print("https://github.com/Anujakhatri/Taxation-system-nepal")
+
+    elif choice == "3":
+        print("https://github.com/sneharitas/currency_converter")
+
+    elif choice == "4":
+        print("https://github.com/binisha77/nepal_remittance_calculator")
+
+    elif choice == "5":
+        print("https://github.com/kopiladevkota/nepal_lagani_kosh")
+
+    elif choice == "6":
+        print("Exiting program...")
+        break
+
+    else:
+        print("Invalid choice")
